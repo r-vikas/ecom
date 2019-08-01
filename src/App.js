0dc17fde5d4843c2c4d796ce8d1f8566149c8c7b
r-vikas <vikasr@outlook.com>
2019-08-01 21:03:47 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Route } from "react-router-dom";
-import ShopPage from './pages/shop/shop.component'
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
-
-
+import Header from "./components/header/header.component";
 
 const jackets = () => (
   <div>
@@ -21,11 +20,13 @@ const sneakers = () => (
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
-      <Route path="/jackets" component={jackets} />
-      <Route path="/sneakers" component={sneakers} />
-    
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/jackets" component={jackets} />
+        <Route path="/sneakers" component={sneakers} />
+      </Switch>
     </div>
   );
 }
